2feed0a10aaa92a661f94963e4e61ab4f63d34f9
carlockk <carlos.virtualdesk@gmail.com>
2025-06-07 03:30:23 -0400

    Configurar CORS para Vercel

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const fs = require('fs');
 dotenv.config({ path: path.join(__dirname, '.env') });
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://asiste-frontend.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json());
 app.use('/uploads', express.static('uploads')); // Servir archivos subidos
 
